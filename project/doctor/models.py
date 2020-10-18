213eab6dce59dee0c6b50f985154e8a87c2b20ea
Akshat <akshatjai258@gmail.com>
2020-10-18 22:53:54 +0530

    Added login logout functionality and started building doctor model

--- project/doctor/models.py
+++ project/doctor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Contact(models.Model):
@@ -10,3 +11,10 @@ class Contact(models.Model):
 	
 	def __str__(self):
 		return "message from "+self.name
+class Doctor(models.Model):
+	user=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+	profile_pic=models.ImageField(upload_to='images/profile', default="")
+	
+	
+	def __str__(self):
+		return self.user.first_name + self.user.last_name
--- project/doctor/views.py
+++ project/doctor/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .models import Contact
+from .models import Contact,Doctor
+from django.contrib.auth  import authenticate,  login, logout
 # Create your views here.
 def home(request):
 	return render(request,'doctor/home.html')
@@ -23,7 +24,10 @@ def contact(request):
 		
 	return render (request,"doctor/contact.html")
 
-
+def dashboard(request,pk):
+	doctor=Doctor.objects.get(id=pk)
+	return redirect(request,'doctor/dashboard.html')
+	
 
 def handleSignup(request):
 	if(request.method=='POST'):
@@ -32,6 +36,8 @@ def handleSignup(request):
 		email=request.POST['email']
 		pass1=request.POST['pass1']
 		pass2=request.POST['pass2']
+		fname=request.POST['first_name']
+		lname=request.POST['last_name']
 		if pass1!=pass2:
 			# TODO: write code...
 			
@@ -42,8 +48,33 @@ def handleSignup(request):
 			
 		
 		myuser=User.objects.create_user(username,email,pass1)
+		myuser.first_name=fname
+		myuser.last_name=lname
+		# myuser.city=city
+		# myuser.tag=[]
+		# for i in tag:
+		# 	myuser.tag.insert(tag)
 		myuser.save()
 		messages.success(request,"Your Account is successfully created!!!")
 		return redirect('doctorHome')
 	
-	
+def handeLogin(request):
+  if request.method=="POST":
+  	loginusername=request.POST['loginusername']
+  	loginpassword=request.POST['loginpassword']
+  	user=authenticate(username= loginusername, password= loginpassword)
+  	if user is not None:
+  		login(request, user)
+  		messages.success(request, "Successfully Logged In")
+  		return redirect("doctorHome")
+  	else:
+  		messages.error(request, "Invalid credentials! Please try again")
+  		return redirect("doctorHome")
+  return HttpResponse("404- Not found")
+  
+  
+def handelLogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('doctorHome')
+
